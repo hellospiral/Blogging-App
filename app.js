0a43738c72401dc9d955f47c25f0b37b38d302c0
Matt Carlson <matthewthomascarlson@gmail.com>
2016-04-28 20:58:27 +0000

    add new and create routes, add new template, add styling

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var Blog = mongoose.model("Blog", blogSchema);
 app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
-
+// INDEX ROUTE
 app.get("/blogs", function(req, res) {
     Blog.find({}, function(err, blogs) {
         if(err) {
@@ -33,6 +33,25 @@ app.get("/blogs", function(req, res) {
             res.render("index", {blogs: blogs});
         }
     });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function(req, res) {
+   //create blog
+   Blog.create(req.body.blog, function(err, newBlog) {
+       if(err){
+           res.render("new");
+       }
+       else{
+           //redirect to index
+           res.redirect("/blogs");
+       }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
